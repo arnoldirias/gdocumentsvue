53e624a0237a2dd11f77fb4e854c8fffa5f5762f
Arnold Irias <arnold.irias@unah.hn>
2020-01-24 01:19:00 -0600

    v1.1.2

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="public/images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="public/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="public/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="public/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="public/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="public/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="public/vendor/select2/select2.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="public/css/util.css">
+	<link rel="stylesheet" type="text/css" href="public/css/main.css">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -25,7 +25,7 @@
 		<div class="container-login100">
 			<div class="wrap-login100 p-l-20 p-r-20 p-t-20 p-b-20">
 
-				<img src="images/img-06.png" alt="" id="imagen2">	
+				<img src="public/images/img-06.png" alt="" id="imagen2">	
 				<form class="login100-form validate-form" id="form" method="POST" action="welcome.php" name="form">
 
 					<span class="login100-form-title p-b-25">INGRESAR A GESTIÓN DE DOCUMENTOS</span>
@@ -72,21 +72,19 @@
 		</footer>
 	</div>
 	
-<!--<script src="vendor/jquery/jquery-3.2.1.min.js"></script>===============================================================================================-->	
-<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="vendor/jquery/jquery-3.2.1.min.js">x3C/script>')</script>
-	
+<!--===============================================================================================-->	
+	<script src="public/vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="public/vendor/bootstrap/js/popper.js"></script>
+	<script src="public/vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
+	<script src="public/vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
 	
 <!-- production version, optimized for size and speed -->
-	<script src="js/vue.js"></script>
-	<script src="js/login.js"></script>
-	<script src="js/core-min.js"></script>
-	<script src="js/sha1-min.js"></script>
+	<script src="public/js/vue.js"></script>
+	<script src="public/js/login.js"></script>
+	<script src="public/js/core-min.js"></script>
+	<script src="public/js/sha1-min.js"></script>
 </body>
 </html>
